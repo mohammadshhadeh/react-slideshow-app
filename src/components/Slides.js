eab26c4a1701daebae0e9a402ad561866c57b09e
mohammadzaidsh10 <mohammad.shhadeh@optimumpartners.co>
2020-09-05 16:30:15 +0300

    React: Slideshow app

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function Slides({slides}) {
-
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+export default class Slides extends Component {
+    render() {
+        const item = this.props.slides[this.props.index];
+        const index = this.props.index;
+        return (
+            <div>
+                <div id="navigation" className="text-center">
+                    <button data-testid="button-restart" onClick={this.props.resetSlides} className="small outlined">
+                        Restart
+                    </button>
+                    <button
+                        data-testid="button-prev"
+                        onClick={this.props.prevSlide}
+                        disabled={index === 0}
+                        className="small"
+                    >
+                        Prev
+                    </button>
+                    <button
+                        onClick={this.props.nextSlide}
+                        disabled={index === this.props.slides.length - 1}
+                        className="small"
+                        data-testid="button-next"
+                    >
+                        Next
+                    </button>
+                </div>
+                <div id="slide" className="card text-center">
+                    <h1 data-testid="title">{item.title}</h1>
+                    <p data-testid="text">{item.text}</p>
+                </div>
             </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+        );
+    }
 }
-
-export default Slides;
